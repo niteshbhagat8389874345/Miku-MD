2908a162a32b7fb0f186220abe14041332950be5
niteshbhagat8389874345 <120049066+niteshbhagat8389874345@users.noreply.github.com>
2022-12-10 16:06:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['917908632051']
+global.OwnerNumber = ['917908632051']
+global.ownertag = ['917908632051']
+global.BotName = "miku"
+global.packname = "night"
+global.author = "By: night"
+global.OwnerName = "night"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.SupportGroupLink = "https://chat.whatsapp.com/ICFZj6RgjYDEURLht1Y5Gy"
 global.sessionName = "session"
 
 
